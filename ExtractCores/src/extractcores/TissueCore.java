6de6fa0d5267fc8742405e2b6bce4c44b10b1f24
Daniel Brüggemann <daniel.brueggemann.mail@gmail.com>
2017-06-25 18:47:12 +0200

    Remove big input file from repo.

--- ExtractCores/src/extractcores/TissueCore.java
+++ ExtractCores/src/extractcores/TissueCore.java
@@ -2,7 +2,7 @@ package extractcores;
 
 import org.opencv.core.Rect;
 
-public class TissueCore {
+public class TissueCore implements Comparable<TissueCore> {
   private Rect boundingBox;
   private int centerX;
   private int centerY;
@@ -27,5 +27,11 @@ public class TissueCore {
   public int getCenterY()
   {
     return centerY;
+  }
+
+  @Override
+  public int compareTo(TissueCore otherCore)
+  {
+    //Wie nach vers. Kriterien sortierbar machen??
   }
 }
